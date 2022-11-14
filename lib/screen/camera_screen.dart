057078b38950b1000494fbc4ab1ffd38cdbcb72f
AUNG533 <aungkyawmon@gmail.com>
2022-11-14 09:37:19 +0700

    SAVING RECORDED VIDEOS TO GALLERY

--- lib/screen/camera_screen.dart
+++ lib/screen/camera_screen.dart
@@ -1,9 +1,10 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_camera/flutter_camera.dart';
 import 'package:video_editing_app/components/colors.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -12,13 +13,14 @@ class CameraScreen extends StatefulWidget {
   State<CameraScreen> createState() => _CameraScreenState();
 }
 
-class _CameraScreenState extends State<CameraScreen> with SingleTickerProviderStateMixin {
-   late AnimationController _controller;
+class _CameraScreenState extends State<CameraScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
-        _controller = AnimationController(vsync: this);
+    _controller = AnimationController(vsync: this);
   }
 
   @override
@@ -27,26 +29,134 @@ class _CameraScreenState extends State<CameraScreen> with SingleTickerProviderSt
     _controller.dispose();
   }
 
+  String pictureButtonText = 'Take a Picture';
+  String videoButtonText = 'Record Video';
+
   @override
   Widget build(BuildContext context) {
-    return FlutterCamera(color: bgColor,
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton.extended(
+              backgroundColor: shadowColor,
+              onPressed: _takePhoto,
+              label: const Text('Take a Photo'),
+            ),
+            const SizedBox(width: 50),
+            FloatingActionButton.extended(
+              backgroundColor: shadowColor,
+              onPressed: _recordVideo,
+              label: const Text('Record a video'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-    onImageCaptured: (value) {
-      final path = value.path;
-      print('Your path is $path');
-      if (path.contains('.jpg')) {
-        showDialog(context: context, builder: (context) {
-          return AlertDialog(
-            content: Image.file(File(path)),
-          );
+  void _takePhoto() async {
+    ImagePicker()
+        .getImage(source: ImageSource.camera)
+        .then((PickedFile? recordedImage) {
+      if (recordedImage != null && recordedImage.path != null) {
+        setState(() {
+          pictureButtonText = 'Saving in progress...';
+        });
+        GallerySaver.saveImage(recordedImage.path, albumName: 'TestImage');
+      }
+    });
+  }
+
+  void _recordVideo() async {
+    ImagePicker()
+        .getVideo(source: ImageSource.camera)
+        .then((PickedFile? recordedVideo) {
+      if (recordedVideo != null && recordedVideo.path != null) {
+        setState(() {
+          videoButtonText = "Saving Video...";
+        });
+        GallerySaver.saveVideo(recordedVideo.path, albumName: 'TestVideo')
+            .then((bool? success) {
+          setState(() {
+            videoButtonText = 'Video Recording Saved Successfully';
+          });
         });
       }
-    },
+    });
+  }
+}
 
-      onVideoRecorded: (value) {
-      final path = value.path;
-      print('video directory is $path');
-      },
+class CameraGalleryScreen extends StatefulWidget {
+  const CameraGalleryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CameraGalleryScreen> createState() => _CameraGalleryScreenState();
+}
+
+class _CameraGalleryScreenState extends State<CameraGalleryScreen> {
+  String pictureButtonText = 'Take a Picture';
+  String videoButtonText = 'Record Video';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Container(
+                child: SizedBox.expand(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(bgColor)),
+                    onPressed: _recordVideo,
+                    child: const Text(
+                      'Take a Picture',
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _takePhoto() async {
+    ImagePicker()
+        .getImage(source: ImageSource.camera)
+        .then((PickedFile? recordedImage) {
+      if (recordedImage != null && recordedImage.path != null) {
+        setState(() {
+          pictureButtonText = 'Saving in progress...';
+        });
+        GallerySaver.saveImage(recordedImage.path, albumName: 'TestImage');
+      }
+    });
+  }
+
+  void _recordVideo() async {
+    ImagePicker()
+        .getVideo(source: ImageSource.camera)
+        .then((PickedFile? recordedVideo) {
+      if (recordedVideo != null && recordedVideo.path != null) {
+        setState(() {
+          videoButtonText = "Saving Video...";
+        });
+        GallerySaver.saveVideo(recordedVideo.path, albumName: 'TestVideo')
+            .then((bool? success) {
+          setState(() {
+            videoButtonText = 'Video Recording Saved Successfully';
+          });
+        });
+      }
+    });
   }
 }
